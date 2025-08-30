6eed2df8c5c7cbbc86398d8578a584404bc34a48
cyanedu_caoxy <caoxy@cyanedu.com>
2025-08-30 19:41:27 +0800

    第二个示例自动导入

---
+++ app/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>欢迎来到 Nuxt 自动导入的世界！</h1>
+    <p>下面的按钮就是一个自动导入的组件，你不需要在任何地方手动注册它。</p>
+    <MyButton />
+    <hr>
+    <p>下面这个函数也是自动导入的，它来自 `composables` 目录。</p>
+    <button @click="showGreeting">打个招呼</button>
+  </div>
+</template>
+
+<script setup>
+const showGreeting = () => {
+  const greeting = useGreeting('老板');
+  alert(greeting);
+}
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+  padding: 2rem;
+  background-color: #f4f4f9;
+}
+h1 {
+  color: #333;
+}
+hr {
+  margin: 2rem 0;
+}
+</style>
---
+++ app/composables/useGreeting.ts
@@ -0,0 +1,3 @@
+export const useGreeting = (name: string) => {
+  return `你好啊，${name}！我是来自 composables 目录的 useGreeting 函数，我也是自动导入的哦！`;
+}
