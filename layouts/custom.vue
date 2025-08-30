459dd19043baeca99c15c0a973932a146a113779
cyanedu_caoxy <caoxy@cyanedu.com>
2025-08-31 07:14:36 +0800

    准备下一个示例layouts -- 以实现

--- layouts/custom.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="bg-gray-100 dark:bg-gray-800 min-h-screen">
-    <slot />
-  </div>
-</template>
-
-<style scoped>
-/* You can add specific styles for this layout here */
-</style>
