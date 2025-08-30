6f6be22cbd92ca064ec27b20ec92377770e6c8b3
cyanedu_caoxy <caoxy@cyanedu.com>
2025-08-30 22:45:40 +0800

    准备重新开始。

--- middleware/log.global.ts
+++
@@ -1,3 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  console.log(`[路由日志] 从: ${from.fullPath} 到: ${to.fullPath}`)
-})
--- server/api/hello.ts
+++
@@ -1,8 +0,0 @@
-
-// 这是一个定义服务器 API 路由的文件
-// 当你访问 /api/hello 时，服务器会返回一个 JSON 对象
-export default defineEventHandler((event) => {
-  return {
-    api: '你好，来自 API 路由！'
-  }
-})
